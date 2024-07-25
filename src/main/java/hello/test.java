b81347a23de8bd193ed71f4c0428b662b3c4e30b
Rohit Singh <81259871+rohitduplo@users.noreply.github.com>
2024-07-25 15:35:16 +0530

    Create test.java

---
+++ src/main/java/hello/test.java
@@ -0,0 +1,17 @@
+public class User {
+    private String firstName;
+    private String lastName;
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+}
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+}
