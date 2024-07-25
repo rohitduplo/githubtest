03e0a22767c6da8bbab027afb47324d35f569604
Rohit Singh <81259871+rohitduplo@users.noreply.github.com>
2024-07-25 15:34:58 +0530

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,23 +1,6 @@
 package hello;
 
 import org.joda.time.LocalTime;
-public class User {
-    private String firstName;
-    private String lastName;
-
-    public String getFullName() {
-        return firstName + " " + lastName;
-    }
-}
-
-public class Employee {
-    private String firstName;
-    private String lastName;
-
-    public String getFullName() {
-        return firstName + " " + lastName;
-    }
-}
 public class HelloWorld {
     public static void main(String[] args) {
       LocalTime currentTime = new LocalTime();
