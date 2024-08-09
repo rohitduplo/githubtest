81de5208bb3f2e56e3d8df9e609e3451b7ca795a
Rohit Singh <81259871+rohitduplo@users.noreply.github.com>
2024-08-09 18:42:07 +0530

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,19 +1,12 @@
 package hello;
 
-import org.joda.time.LocalDate; // Intentionally import LocalDate instead of LocalTime
-
+import org.joda.time.LocalTime;
 public class HelloWorld {
     public static void main(String[] args) {
-        LocalDate currentTime = new LocalDate(); // LocalDate instead of LocalTime
-        System.out.println("The current local time is: " + currentTime);
+      LocalTime currentTime = new LocalTime();
+		  System.out.println("The current local time is: " + currentTime);
 
         Greeter greeter = new Greeter();
-        
-        // Add a redundant null check that will never be true
-        if (greeter == null) {
-            System.out.println("Greeter is null");
-        } else {
-            System.out.println(greeter.sayHello()); // Correct method call
-        }
+        System.out.println(greeter.sayHello());
     }
 }
